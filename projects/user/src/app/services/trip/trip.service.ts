8c54bcce29974fd1c376f3e7313a76a23320e53d
M-bayoumi <101809237+M-bayoumi@users.noreply.github.com>
2023-11-05 04:17:21 +0200

    2023-11-5

---
+++ projects/user/src/app/services/trip/trip.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { IFilterTrip } from '../../models/ifilter-trip';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripService {
+
+  constructor(private http: HttpClient) { }
+
+  FilterTrips(filterTrip:IFilterTrip) {
+    return this.http.post(`${environment.Api}/trips/search`,filterTrip);
+  }
+}
